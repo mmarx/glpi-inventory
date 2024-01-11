df1b5ed6f1ede54763ad12e94c8e789295f424e3
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-01-11 13:59:56 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "GLPI inventory for NixOS";
+
+  inputs = {
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs, utils, ... }:
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      overlays = rec {
+        glpi-inventory = final: prev: {
+          glpi-agent = final.callPackage ./glpi-agent { };
+        };
+        default = glpi-inventory;
+      };
+
+      nixosModules = { glpi-inventory = ./glpi-inventory.nix; };
+
+      channels.nixpkgs.overlaysBuilder = channels: [ self.overlays.default ];
+
+      outputsBuilder = channels: {
+        packages = utils.lib.exportPackages { inherit (self.overlays) default; }
+          channels;
+      };
+    };
+}
