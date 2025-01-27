dd3511453c81adbb3965f5dd089fe6956c37d318
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-01-27 16:26:02 +0100

    Bump to upstream 1.12
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- glpi-agent/default.nix
+++ glpi-agent/default.nix
@@ -18,13 +18,13 @@
 }:
 perlPackages.buildPerlPackage rec {
   pname = "glpi-agent";
-  version = "1.11";
+  version = "1.12";
 
   src = fetchFromGitHub {
     owner = "glpi-project";
     repo = "glpi-agent";
     rev = "${version}";
-    hash = "sha256-WdQ+/ZnMCRqLZK64oJNoR9dtMPq+CghsA8NUwt3EpjA=";
+    hash = "sha256-P3IMq+/YvfJULgP8+xnRmpASA6AS/1lr8hglK5UCZIo=";
   };
 
   patches = [
