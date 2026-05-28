24b5ced6766fa6d712cb5b07c3a7fd580745a850
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2026-05-28 15:30:55 +0200

    Bump package

--- glpi-agent/default.nix
+++ glpi-agent/default.nix
@@ -18,13 +18,13 @@
 }:
 perlPackages.buildPerlPackage rec {
   pname = "glpi-agent";
-  version = "1.16";
+  version = "1.17";
 
   src = fetchFromGitHub {
     owner = "glpi-project";
     repo = "glpi-agent";
     rev = "${version}";
-    hash = "sha256-JocNClbFH6fSZJDAVQCPaRvHrkLboNbQVTpyHjOV7go=";
+    hash = "sha256-ug3/ullvEn98UUg4fzDQl5PjVFlbgbaIiz0tuWz9XeA=";
   };
 
   patches = [
